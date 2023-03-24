d1e02d9d66e70fe25ba8cd230141b2bc4c50be08
Douglas de Jesus <dougecomp@gmail.com>
2023-03-24 20:34:07 -0300

    .

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.sistemadegerenciamento {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.sistemadegerenciamento to javafx.fxml;
-    exports com.example.sistemadegerenciamento;
-}
