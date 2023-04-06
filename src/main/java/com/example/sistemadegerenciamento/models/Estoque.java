19623170cf0705b814e34c6dbcacef478c56610e
Douglas de Jesus <dougecomp@gmail.com>
2023-04-06 14:36:03 -0300

    :sparkles: feat: Criação de padrão singleton e métodos de entrada e saída de peças

--- src/main/java/com/example/sistemadegerenciamento/models/Estoque.java
+++ src/main/java/com/example/sistemadegerenciamento/models/Estoque.java
@@ -1,20 +1,39 @@
 package com.example.sistemadegerenciamento.models;
 import java.util.ArrayList;
-//Padrão singleton
-//Alterações: definição das duas coleções e remoção do construtor.
+import java.util.HashMap;
+
+/**
+ * Classe responsável por armazenar informações do almoxorifado: peças e ordens de compra.
+ * */
 
 public class Estoque {
 
-	//Definição das duas coleções.
-    private ArrayList<Peca> pecas = new ArrayList();
-	private ArrayList<OrdemCompra> ordensCompra = new ArrayList();
-
-	//Estoque pode surgir sem peças e sem ordemDeCompra.
-    public Estoque(){
+    //Definição das duas coleções.
+    //HashMap que guarda a peça e quantidade.
+    private HashMap<Peca, Integer> quantidadePecas = new HashMap<Peca, Integer>();
+    private ArrayList<OrdemCompra> ordensCompra = new ArrayList();
+    //Atributo estático para armazenar a única instância da classe
+    private static Estoque instancia;
+    //Construtor privado para impedir que outras classes instanciem a classe diretamente.
+    private Estoque(){
 
     }
+    //Método estático que retorna a instância única da classe. Padrão Singleton.
+    public static Estoque getInstance() {
+        if (instancia == null) {
+            instancia = new Estoque();
+        }
+        return instancia;
+    }
+    public void addOrdemCompra(OrdemCompra ordemCompra){
+        ordensCompra.add(ordemCompra);
+        quantidadePecas.put(ordemCompra.getPeca(), ordemCompra.getQuantidade());
+    }
+    public void decrementaPeca(Peca peca){
+        quantidadePecas.put(peca, quantidadePecas.get(peca)-1);
+    }
+    public HashMap getEstoque(){
+        return quantidadePecas;
+    }
 
-	//Falta definir adicionar peças ao estoque através da ordemDeCompra.
-	//Falta remover peça de acordo com retirada.
 }
- // falta construtor no padrão singleton
