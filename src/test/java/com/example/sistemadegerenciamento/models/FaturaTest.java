8adc5c8ead1ec15516964ad88d9478a9aa8a1b3e
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-04-15 17:07:28 -0300

    :test_tube: test: Classe fatura quase completa

--- src/test/java/com/example/sistemadegerenciamento/models/FaturaTest.java
+++ src/test/java/com/example/sistemadegerenciamento/models/FaturaTest.java
@@ -2,6 +2,8 @@ package com.example.sistemadegerenciamento.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FaturaTest {
@@ -17,19 +19,42 @@ class FaturaTest {
 
     @Test
     void getValorTotal() {
+        double valorTotal = 200.0;
+        Fatura fatura = new Fatura(valorTotal,1);
+        assertEquals(valorTotal, fatura.getValorTotal(), 0.0);
     }
 
     @Test
     void getPagamentos() {
+        Pagamento pag1 = new Pagamento("Credito", 200.0, 1);
+        Pagamento pag2 = new Pagamento("Debito", 100.0, 2);
+        Fatura fatura = new Fatura(300.0, 1);
+        fatura.addPagamento(pag1);
+        fatura.addPagamento(pag2);
+        ArrayList<Pagamento> pags = fatura.getPagamentos();
+        assertEquals(2, pags.size());
+        assertEquals(pag1, pags.get(0));
+        assertEquals(pag2, pags.get(1));
+
     }
 
     @Test
     void getOrdemID() {
+        int Id = 123;
+        Fatura fatura = new Fatura(100.0, Id);
+        assertEquals(Id, fatura.getOrdemID());
     }
+
 
     @Test
     void setOrdemID() {
+        int IdInicial = 1;
+        int IdFinal = 2;
+        Fatura fatura = new Fatura(100.0, IdInicial);
+        fatura.setOrdemID(IdFinal);
+        assertEquals(IdFinal, fatura.getOrdemID());
     }
+
 
     @Test
     void getValorPago() {
