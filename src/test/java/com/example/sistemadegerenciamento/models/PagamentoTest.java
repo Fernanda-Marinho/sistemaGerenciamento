4c9f98a77d3767f26baa7bd763432ca002e7939f
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-04-15 17:56:22 -0300

    :test_tube: test: Classe Pagamento

--- src/test/java/com/example/sistemadegerenciamento/models/PagamentoTest.java
+++ src/test/java/com/example/sistemadegerenciamento/models/PagamentoTest.java
@@ -8,13 +8,19 @@ class PagamentoTest {
 
     @Test
     void getTipoPagamento() {
+        Pagamento pagamento = new Pagamento("cartão", 50.0, 1);
+        assertEquals("cartão", pagamento.getTipoPagamento());
     }
 
     @Test
     void getValor() {
+        Pagamento pagamento = new Pagamento("cartão", 50.0, 1);
+        assertEquals(50.0, pagamento.getValor(), 0.0);
     }
 
     @Test
     void getFaturaID() {
+        Pagamento pagamento = new Pagamento("cartão", 50.0, 1);
+        assertEquals(1, pagamento.getFaturaID());
     }
 }
