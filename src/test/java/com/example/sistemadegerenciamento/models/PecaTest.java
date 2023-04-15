4cd95b24aa74088c8230231dec6826500a66e3a1
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-04-15 20:36:54 -0300

    :test_tube: test: Classe Peça

--- src/test/java/com/example/sistemadegerenciamento/models/PecaTest.java
+++ src/test/java/com/example/sistemadegerenciamento/models/PecaTest.java
@@ -8,17 +8,27 @@ class PecaTest {
 
     @Test
     void getNome() {
+        Peca peca = new Peca("Peça1", 20.0);
+        assertEquals("Peça1", peca.getNome());
     }
 
     @Test
     void setNome() {
+        Peca peca = new Peca("Peça1", 20.0);
+        peca.setNome("Peça2");
+        assertEquals("Peça2", peca.getNome());
     }
 
     @Test
     void getValor() {
+        Peca peca = new Peca("Peça1", 20.0);
+        assertEquals(20.0, peca.getValor(), 0.0);
     }
 
     @Test
     void setValor() {
+        Peca peca = new Peca("Peça1", 20.0);
+        peca.setValor(40.0);
+        assertEquals(40.0, peca.getValor(), 0.0);
     }
 }
