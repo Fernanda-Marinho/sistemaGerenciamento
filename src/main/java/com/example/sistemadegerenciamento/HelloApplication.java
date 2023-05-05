0d3f4546585a51268bcf3c2d1d691d3505ebdbcf
Douglas de Jesus <dougecomp@gmail.com>
2023-05-04 22:51:05 -0300

    :sparkles: feat: Adicionando recursos de interface gráfica

--- src/main/java/com/example/sistemadegerenciamento/HelloApplication.java
+++ src/main/java/com/example/sistemadegerenciamento/HelloApplication.java
@@ -13,14 +13,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Gerenc. Microcomputadores");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        MainController.main(args);
-        //launch();
+        //MainController.main(args);
+        launch();
     }
 }
--- src/main/java/com/example/sistemadegerenciamento/HelloController.java
+++ src/main/java/com/example/sistemadegerenciamento/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.sistemadegerenciamento;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Interface em manutenção!\nVolte mais tarde.");
+    void btnMenu(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnOk(ActionEvent event) {
+
+    }
+
 }
